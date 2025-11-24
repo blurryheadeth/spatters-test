82de10b4a649b5808b7d77674e1d2d4e97c0929f
Glen Albó <glenalbo@gmail.com>
2025-11-24 17:43:17 +0100

    Downgrade to Hardhat v2 for stability and successfully deploy to Sepolia testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -19,12 +19,10 @@ const config: HardhatUserConfig = {
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.public.blastapi.io",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-      chainId: 11155111,
     },
     mainnet: {
       url: process.env.MAINNET_RPC_URL || "https://eth.public-rpc.com",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-      chainId: 1,
     },
   },
   paths: {
@@ -37,6 +35,9 @@ const config: HardhatUserConfig = {
     outDir: "typechain",
     target: "ethers-v6",
     alwaysGenerateIntoStructs: true,
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
   },
 };
 
